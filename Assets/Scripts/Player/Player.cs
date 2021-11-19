96045137cb04aeec8547bdeee33d5c2e93e70bcf
Bucks1bunny <46489335+Bucks1bunny@users.noreply.github.com>
2021-11-19 15:51:28 +0200

    3Level
    Button press and box trigger

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,22 +1,58 @@
 using UnityEngine;
-using System.Collections.Generic;
-using System.Collections;
+using UnityEngine.Events;
 
-public class Player : MonoBehaviour,IDamageable
+public class Player : MonoBehaviour//,IDamageable
 {
+    [SerializeField] private Transform objectContainer;
+    Camera cam;
+    Ray ray;
+    RaycastHit hit;
 
-    public HealthBar healthBar;
-    public float maxHealth = 100;
-    public static float currentHealth;
-    void Start()
+    private bool isCarry = false;
+    private int rangeToObject = 2;
+    private void Start()
     {
+        cam = Camera.main;
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Keypad1)) { }
+        // Interaction with objects
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray,out hit, rangeToObject))
+        {
+            if(hit.collider.CompareTag("Button") && Input.GetKeyDown(KeyCode.E)) PressButton();
+
+            if (hit.collider.CompareTag("Object") && Input.GetKeyDown(KeyCode.E))
+            {
+                isCarry = !isCarry;
+            }
+        }
+        //carring objects
+        if (isCarry)
+            CarryObjects();
+        else hit.rigidbody.isKinematic = false;
     }
+    public UnityEvent PlayerPress;
+    private void PressButton()
+    {
+         PlayerPress?.Invoke();
+    }
+    private void CarryObjects()
+    {
+        hit.rigidbody.isKinematic = true;
+        hit.collider.transform.position = objectContainer.position;
+    }
+    /*public UnityEvent OnBoxInteract;
+    private void BoxInteract()
+    {
+        OnBoxInteract?.Invoke();
+    }*/
+
+    public HealthBar healthBar;
+    public float maxHealth = 100;
+    public static float currentHealth;
     public void Heal(float healAmount)
     {
         currentHealth += healAmount;
